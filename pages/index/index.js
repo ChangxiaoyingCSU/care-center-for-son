c0806a39c30c0aab6e61ab067c3dcb754911939f
Changxiaoying <331980235@qq.com>
2021-07-14 00:36:55 +0800

    首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,6 +10,7 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     canIUseGetUserProfile: false,
     canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    
   },
   // 事件处理函数
   bindViewTap() {
@@ -44,5 +45,7 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-  }
+  },
+  
 })
+
