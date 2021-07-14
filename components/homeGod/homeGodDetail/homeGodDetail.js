600aec7da0622da29c1f6f8e6b2353124fba2a86
张琰 <1446174394@qq.com>
2021-07-14 16:18:49 +0800

    修改护工数据

--- components/homeGod/homeGodDetail/homeGodDetail.js
+++ components/homeGod/homeGodDetail/homeGodDetail.js
@@ -8,11 +8,11 @@ Page({
 	msg:"位置:此房,前面是小区的绿化带,景观绝好,下楼2m出小区,上班方便。+户型:1室1卫0阳台45m²,保养的太好了,看房方便。装修:普通服务:资深经纪人24h服务热线*承诺:所有离源均为真实有效房源,恭候您的来电咨询！",
 	data: {
 		info:{
-			name: "周某人",
-			img:"../../../../assets/home/Headportrait/s01.png",
+			name: "客服",
+			img:"../../../assets/home/Headportrait/s01.png",
 			phone:10086
 		},
-		images1:"../../../../assets/0bb3c290e353d785fa1bd27391cab43.png",
+		images1:"../../../assets/0bb3c290e353d785fa1bd27391cab43.png",
 		images: [
 			"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1587310939038&di=e248b575af0fe89bfd3dda3e60d3e35b&imgtype=0&src=http%3A%2F%2Fimg5.cache.netease.com%2Fhouse%2F2014%2F6%2F5%2F2014060513434168c46_550.jpg",
 			"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1587310939038&di=e248b575af0fe89bfd3dda3e60d3e35b&imgtype=0&src=http%3A%2F%2Fimg5.cache.netease.com%2Fhouse%2F2014%2F6%2F5%2F2014060513434168c46_550.jpg",
@@ -20,97 +20,97 @@ Page({
 		],
 		// name不赋值  只赋值price
 		houseInfo: [{
-				name: "单价",
-				price: "56914.5元/m²"
+				name: "编号",
+				price: "1"
 			},
 			{
-				name: "挂牌",
-				price: "2019.07.19"
+				name: "姓名",
+				price: "小陈"
 			},
 			{
-				name: "面积",
-				price: "62.55m²"
+				name: "性别",
+				price: "女"
 			},
 			{
-				name: "户型",
-				price: "2室2厅1卫"
+				name: "年龄",
+				price: "56"
 			},
 			{
-				name: "朝向",
-				price: "东"
+				name: "工资",
+				price: "8K"
 			},
 			{
-				name: "楼层",
-				price: "6/16"
+				name: "性格",
+				price: "负责、有耐心"
 			},
 			{
-				name: "装修",
-				price: "精装"
+				name: "服务编号",
+				price: "1"
 			},
 			{
-				name: "权属",
-				price: "商品房"
+				name: "服务等级",
+				price: "高级"
 			},
 			{
-				name: "用途",
-				price: "普通住宅"
+				name: "学历",
+				price: "普通本科"
 			},
 			{
-				name: "电梯",
-				price: "有电梯"
+				name: "婚姻状况",
+				price: "已婚"
 			},
 			{
-				name: "首付预算",
-				price: "首付约 25万元"
+				name: "服务年限",
+				price: "待商定"
 			},
 			{
-				name: "小区",
-				price: "左方坊"
+				name: "服务内容",
+				price: "照料起居"
 			},
 		],
 		observe: [{
-				name: "房源户型",
-				price: "2室1厅1卫"
+				name: "服务地址",
+				price: "长沙市天心区韶山南路22号"
 			},
 			{
 				name: "建筑面积",
 				price: "62.55m²"
 			},
 			{
-				name: "套内面积",
-				price: "50.32m²"
+				name: "成立性质",
+				price: "私人所有"
 			},
 			{
-				name: "户型结构",
-				price: "平层"
+				name: "创立时间",
+				price: "2008年"
 			},
 			{
-				name: "梯户结构",
-				price: "平层"
+				name: "所属地区",
+				price: "湖南省长沙市"
 			},
 			{
-				name: "梯户结构",
-				price: "六梯三十五户"
+				name: "创立类别",
+				price: "梅岭社区颐养中心"
 			},
 			{
-				name: "供暖方式",
-				price: "自供暖"
+				name: "发展历史",
+				price: "由湖南三所养老中心合并"
 			},
 			{
-				name: "房屋年限",
-				price: "满五年"
+				name: "服务特色",
+				price: "智能颐养"
 			},
 			{
-				name: "房屋用途",
-				price: "普通住宅"
+				name: "专业模式",
+				price: "一体化设计、系统管理"
 			},
 			{
-				name: "交易权属",
-				price: "商品房"
+				name: "现任董事",
+				price: "常晓颖"
 			},
 			{
-				name: "产权所属",
-				price: "非共有"
+				name: "联系电话",
+				price: "15573385311"
 			},
 		],
 		excellentHouse: [{
@@ -124,7 +124,7 @@ Page({
 				specifications: '2室1厅',
 				squaremeter: '67.7',
 				homesuper: '精装修',
-				avatar: '../../assets/home/Headportrait/s01.png',
+				avatar: '../../../assets/home/Headportrait/s01.png',
 				type: '个人'
 			},
 			{
@@ -137,7 +137,7 @@ Page({
 				specifications: '3室1厅',
 				squaremeter: '77.7',
 				homesuper: '精装修',
-				avatar: '../../assets/home/Headportrait/s02.png',
+				avatar: '../../../assets/home/Headportrait/s02.png',
 				type: '个人'
 			}
 		],
