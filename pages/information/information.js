63743fb76916de859c6a0947c054be520fe49188
张琰 <1446174394@qq.com>
2021-07-15 01:45:31 +0800

    修改了通知

--- pages/information/information.js
+++ pages/information/information.js
@@ -21,6 +21,75 @@ Page({
     ],
     weather: { 'wea_img': 'qing'},//实况天气
     weatherweek:[],//七日天气
+    excellentHouse: [{
+      id: '1',
+      zd: '1',
+      img: '/assets/doctor/1.jpg',
+      title: '梅岭社区颐养中心服务，营养配餐、清洁卫生、居室安全',
+      stmel: '天心区',
+      stmels: '梅岭社区',
+      number: '7',
+      specifications: '从业7年',
+      squaremeter: '93.7',
+      homesuper: '任劳任怨',
+      avatar: '../../../assets/home/Headportrait/s01.png',
+      type: '个人'
+    },
+    {
+      id: '2',
+      img: '/assets/doctor/2.jpg',
+      title: '梅岭社区颐养中心服务，护理卫生、衣物清洗、协助沐浴',
+      stmel: '天心区',
+      stmels: '梅岭社区',
+      number: '9',
+      specifications: '从业3年',
+      squaremeter: '89.7',
+      homesuper: '热情干练',
+      avatar: '../../../assets/home/Headportrait/s02.png',
+      type: '个人'
+    }
+  ],
+  personalHousing: [{
+      id: '1',
+      zd: '1',
+      img: '/assets/doctor/3.jpg',
+      title: '梅岭社区颐养中心服务，健康保健，常见老年病的护理，正确掌握给药途径',
+      stmel: '天心区',
+      stmels: '梅岭社区',
+      number: '6',
+      specifications: '从业4年',
+      squaremeter: '91.5',
+      homesuper: '知识充沛',
+      avatar: '../../assets/home/Headportrait/s01.png',
+      type: '企业'
+    },
+    {
+      id: '2',
+      img: '/assets/doctor/4.jpg',
+      title: '梅岭社区颐养中心服务，帮助老人洗脸、擦身、洗澡、喂饭、锻炼身体',
+      stmel: '天心区',
+      stmels: '梅岭社区',
+      number: '5',
+      specifications: '从业2年',
+      squaremeter: '93.2',
+      homesuper: '细致用心',
+      avatar: '../../assets/home/Headportrait/s02.png',
+      type: '个人'
+    },
+    {
+      id: '3',
+      img: '/assets/doctor/1.jpg',
+      title: '梅岭社区颐养中心服务，沟通陪伴，针对病患存在和潜在的心理问题，给予关怀、支持和帮助',
+      stmel: '天心区',
+      stmels: '梅岭社区',
+      number: '10',
+      specifications: '从业6年',
+      squaremeter: '97.8',
+      homesuper: '学历优秀',
+      avatar: '../../assets/home/Headportrait/s03.png',
+      type: '企业'
+    }
+  ]
   },
 
   /**
